5aeb02036398fad87e7f6a2b9e5ac5abd492e266
Nemanja Zoric <nemanja.zoric1.nz@gmail.com>
2018-11-27 16:06:52 +0100

    Added order by pipe for checklists

--- src/pages/checklists/checklists.module.ts
+++ src/pages/checklists/checklists.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ChecklistsPage } from './checklists';
 import { RoundProgressModule } from 'angular-svg-round-progressbar';
+import {OrderPipe} from "../../pipes/order/order";
 
 @NgModule({
   declarations: [
-    ChecklistsPage,
+    ChecklistsPage, OrderPipe
   ],
   imports: [
     IonicPageModule.forChild(ChecklistsPage), RoundProgressModule
