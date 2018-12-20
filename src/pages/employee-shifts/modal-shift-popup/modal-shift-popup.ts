5b6bee70fd1c0c524e49926d5f8963617db12405
Nemanja Zoric <nemanja.zoric1.nz@gmail.com>
2018-12-20 11:05:41 +0100

    Modal shift popup code ref.

--- src/pages/employee-shifts/modal-shift-popup/modal-shift-popup.ts
+++ src/pages/employee-shifts/modal-shift-popup/modal-shift-popup.ts
@@ -2,18 +2,13 @@ import {Component} from '@angular/core';
 import {AlertController, NavController, NavParams, ViewController} from 'ionic-angular';
 import {ShiftsProvider} from "../../../providers/shifts/shifts";
 
-/**
- * Generated class for the ModalShiftPopupPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 @Component({
   selector: 'page-modal-shift-popup',
   templateUrl: 'modal-shift-popup.html',
 })
 export class ModalShiftPopupPage {
 
+  // vars
   public request: any;
   public title: any;
   public shift: any;
@@ -21,38 +16,53 @@ export class ModalShiftPopupPage {
   constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, public shiftProvider: ShiftsProvider, public alertCtrl: AlertController) {
   }
 
+  /**
+   * On modal load get params data
+   */
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ModalShiftPopupPage');
     this.title = this.navParams.get('popupTitle');
     this.request = this.navParams.get('request');
     this.shift = this.navParams.get('shift');
-    console.log(this.shift);
   }
 
+  /**
+   * Set long month text
+   * @param date
+   */
   public persistLongMonth(date) {
     date = new Date(date);
     return date.toLocaleString('en-US', {month: 'long'});
   }
 
+  /**
+   * Set long weekday text
+   * @param date
+   */
   public persistLongWeekDay(date) {
     date = new Date(date);
     return date.toLocaleString('en-US', {weekday: 'long'});
   }
 
+  /**
+   * Dismiss modal with false parameter
+   */
   dismiss() {
     this.viewCtrl.dismiss(false);
   }
 
+  /**
+   * On click, if drop -> cancel request, else -> send request
+   * Pass back true
+   * @param shift
+   */
   clickRequest(shift) {
     if(shift.drop !== undefined) {
       // cancel request
-      console.log('cancel request');
       this.shiftProvider.cancelRequest(shift.requestId, this.request).then(response => {
         this.viewCtrl.dismiss(true);
       })
     } else {
       // send request
-      console.log('send request');
       this.shiftProvider.sendRequest(localStorage.getItem('currentPersonId'), this.request).then((response:any) => {
         if(response.success !== undefined && !response.success) {
           // show message
@@ -67,11 +77,8 @@ export class ModalShiftPopupPage {
           alert.present();
           return;
         }
-        console.log(response.success);
-
         this.viewCtrl.dismiss(true);
       })
     }
   }
-
 }
