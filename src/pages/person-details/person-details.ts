9885105045b300fd0c0d4aeb603b0912d57db2fe
Nemanja Zoric <nemanja.zoric1.nz@gmail.com>
2018-12-24 15:39:28 +0100

    Added model to form on person details page

--- src/pages/person-details/person-details.ts
+++ src/pages/person-details/person-details.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {EmployeeProvider} from "../../providers/employee/employee";
 
 /**
  * Generated class for the PersonDetailsPage page.
@@ -14,12 +15,55 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'person-details.html',
 })
 export class PersonDetailsPage {
+  public currentPersonId: any = localStorage.getItem('currentPersonId');
+  public currentCompanyId: any = localStorage.getItem('currentCompanyId');
+  public currentCorporateId: any = localStorage.getItem('currentCorporateId');
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  // page details form
+  public user_email: string;
+  public home_phone: string;
+  public mobile_phone: string;
+  public address_line_1: string;
+  public address_line_2: string;
+  public state_country: string;
+  public country: any;
+  public emergency_contact_name: string;
+  public emergency_phone: any;
+  public emergency_address: any;
+  public emergency_city: any;
+  public emergency_country: any;
+  public emergency_medical_conditions: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private employeeProvider: EmployeeProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PersonDetailsPage');
+    this.getEmployee();
+  }
+
+  public getEmployee() {
+    this.employeeProvider.getEmployee(this.currentCompanyId, this.currentPersonId).then((result: any) => {
+      console.log(result);
+      if(result) {
+        this.user_email = result.email;
+        this.home_phone = result.phone;
+        this.mobile_phone = result.mobile;
+        this.address_line_1 = result.address1;
+        this.address_line_2 = result.address2;
+        this.state_country = result.address4;
+        this.country = result.country['id'];
+
+        this.emergency_contact_name = result.emergencyContactName;
+        this.emergency_address = result.emergencyAddress1;
+        this.emergency_phone = result.emergencyContactPhone;
+        this.emergency_country = result.country['id'];
+        this.emergency_city = result.emergencyAddress4;
+        this.emergency_medical_conditions = result.medicalConditions;
+      }
+    }).catch(error => {
+      console.log(error);
+    })
   }
 
 }
