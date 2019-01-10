f67228f4db64dfa4dead3afc49383f638131c189
Nemanja Zoric <nemanja.zoric1.nz@gmail.com>
2019-01-10 11:56:51 +0100

    Dismiss function for modal

--- src/pages/shift-availability/modal-shift-availability/modal-shift-availability.ts
+++ src/pages/shift-availability/modal-shift-availability/modal-shift-availability.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {NavController, NavParams, ViewController} from 'ionic-angular';
 
 @Component({
   selector: 'page-modal-shift-availability',
@@ -9,13 +9,17 @@ export class ModalShiftAvailabilityPage {
   public status: string;
   public isAvailable: boolean;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ModalShiftAvailabilityPage');
     this.status = this.navParams.get('popupTitle');
     this.isAvailable = this.status === 'PREFERRED';
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
   }
 
 }
