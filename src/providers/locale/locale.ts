f55f4e6393247feabef978d6d010ca5a130b7b62
Nemanja Zoric <nemanja.zoric1.nz@gmail.com>
2019-01-28 11:39:12 +0100

    Some comments to locale provider

--- src/providers/locale/locale.ts
+++ src/providers/locale/locale.ts
@@ -8,16 +8,26 @@ export class LocaleProvider {
   constructor(public http: HttpClient) {
   }
 
+  /**
+   * Returns default fallback locale, if for some reason neededd
+   */
   public getFallbackLocale() {
     return 'en-US';
   }
 
+  /**
+   * Returns device locale
+   */
   public getDeviceLocale() {
     return window.navigator.language;
   }
 
+  /**
+   * Returns converted date, from object to string in provided format, eg. 'YYYY-MM-DD'
+   * @param date
+   * @param string
+   */
   public convert(date, string) {
     return moment(date).format(string);
   }
-
 }
